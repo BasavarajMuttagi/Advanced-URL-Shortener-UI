5aeea97b0a6c80ef77ef4c8ac5c03943ac166d71
BasavarajMuttagi <basavaraj2770@gmail.com>
2025-01-28 19:11:05 +0530

    feat: add UI components for creating short URLs with form validation

---
+++ src/components/CreateShortURL.tsx
@@ -0,0 +1,138 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+const formSchema = z.object({
+  longUrl: z.string().url({
+    message: "Please enter a valid URL",
+  }),
+  topic: z
+    .string()
+    .min(2, {
+      message: "Topic must be at least 2 characters",
+    })
+    .optional(),
+  customAlias: z
+    .string()
+    .min(3, { message: "Alias must be at least 3 characters" })
+    .regex(/^[a-zA-Z0-9-_]+$/, {
+      message:
+        "Alias can only contain letters, numbers, hyphens and underscores",
+    })
+    .optional(),
+});
+
+function CreateShortURL() {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      longUrl: "",
+      topic: "",
+      customAlias: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    setOpen(false);
+    form.reset();
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen} modal={true}>
+      <DialogTrigger asChild>
+        <Button>Shorten URL</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="flex flex-row items-center justify-between">
+          <DialogTitle className="text-lg font-semibold">
+            Create Short URL
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="longUrl"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="!text-zinc-600">URL</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter the URL you want to shorten"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="topic"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="!text-zinc-600">
+                    Topic (Optional)
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Categorize your URL with a topic"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="customAlias"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="!text-zinc-600">
+                    Custom Alias (Optional)
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Create a custom alias for your shortened URL"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex justify-end">
+              <Button type="submit">Submit</Button>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateShortURL;
